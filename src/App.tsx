ce6c0f363f9882201ae9ef986ff074cff1559413
Ian Flynn <ianflynnwork@gmail.com>
2023-06-27 10:46:24 -0400

    aboutus boiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+import AboutUs from './components/AboutUs';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import OurWork from './components/OurWork';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <AboutUs />
+      <OurWork />
     </>
   );
 }
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+  return (
+    <div id='about-us'>
+      <h2>about us here</h2>
+    </div>
+  );
+};
+
+export default AboutUs;
