d64c17648859ce9165241513f1756a14964ce4ce
Ian Flynn <ianflynnwork@gmail.com>
2023-06-27 13:28:00 -0400

    html form basics

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,17 @@
-const Contact = () => {
+import { useState } from 'react';
+
+interface FormValues {
+  name: string;
+  email: string;
+  description: string;
+}
+
+const Contact: React.FC = () => {
+  const [formValues, setFormValues] = useState<FormValues>({
+    name: '',
+    email: '',
+    description: '',
+  });
   return (
     <div id='contact'>
       <div id='contact-blurb'>
@@ -8,8 +21,18 @@ const Contact = () => {
           Let's get started!
         </p>
       </div>
-      <form>
-        <input type='text' />
+      <form id='contact-form'>
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' />
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' required={true} />
+        <label htmlFor='description'>
+          Tell us a little bit about your project
+        </label>
+        <input type='text' name='description' />
+        <button type='submit' id='contact-sumbit-button'>
+          Send
+        </button>
       </form>
     </div>
   );
