e0b8ac1d1ac6e19b0abfe18bd6c0b9a93060cd12
Jeb Stone <jeb.b.stone@gmail.com>
2023-06-27 12:12:29 -0700

    Adds Headline to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,11 @@
 const Hero = () => {
+  function handleScroll() {
+    console.log('scrolling');
+  }
   return (
     <div id='hero'>
       <h2>hero here</h2>
+      <h1>Brutally Simple, Dynamic Digital Solutions</h1>
     </div>
   );
 };
