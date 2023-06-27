a53fff80118790dc6305d2e557d3b76887355452
Jeb Stone <jeb.b.stone@gmail.com>
2023-06-27 13:54:04 -0700

    Adds headline to hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React, { useCallback } from 'react';
-
-
 import './App.css';
 import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
@@ -9,24 +6,11 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import OurWork from './components/OurWork';
 
-// Particles imports
-import Particles from 'react-particles';
-import { loadFull } from 'tsparticles';
-import { Engine } from 'tsparticles-engine';
-import loginParticles from './assets/login-particles.json';
-
-
-
 function App() {
-  // Initialze background particles
-  const particlesInit = useCallback(async (engine: Engine): Promise<void> => {
-    await loadFull(engine);
-  }, []);
 
-  const particleOptions: any = loginParticles;
   return (
     <>
-      <Particles options={particleOptions} init={particlesInit} />
+      
       {/* header */}
       <Navbar />
       {/* main */}
