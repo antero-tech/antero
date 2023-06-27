648c339b322082ec5986fd00e35a82dcbfde9f5b
Jeb Stone <jeb.b.stone@gmail.com>
2023-06-27 12:00:45 -0700

    adds particles to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React, { useCallback } from 'react';
+
+
 import './App.css';
 import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
@@ -6,9 +9,24 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import OurWork from './components/OurWork';
 
+// Particles imports
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
+import { Engine } from 'tsparticles-engine';
+import loginParticles from './assets/login-particles.json';
+
+
+
 function App() {
+  // Initialze background particles
+  const particlesInit = useCallback(async (engine: Engine): Promise<void> => {
+    await loadFull(engine);
+  }, []);
+
+  const particleOptions: any = loginParticles;
   return (
     <>
+      <Particles options={particleOptions} init={particlesInit} />
       {/* header */}
       <Navbar />
       {/* main */}
