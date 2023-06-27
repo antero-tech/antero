8f71888cc59f4dc846ae79cdc0e5d00cf9798da3
Ian Flynn <ianflynnwork@gmail.com>
2023-06-27 10:20:06 -0400

    navbar basic holder code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
