71e79032310bd75480ce63a5735aea6ed0abb3c5
Ian Flynn <ianflynnwork@gmail.com>
2023-06-27 15:34:08 -0400

    add container for cards to flex inside of

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -1,7 +1,31 @@
-const OurWork = () => {
+import Card from './Card';
+
+const OurWork: React.FC = () => {
   return (
     <div id='our-work'>
       <h2>our work here</h2>
+      <div className='container'>
+        <Card
+          title='example1'
+          imgSrc='https://picsum.photos/200/300'
+          imgAlt='summer'
+        />
+        <Card
+          title='example1'
+          imgSrc='https://picsum.photos/200/300'
+          imgAlt='summer'
+        />
+        <Card
+          title='example1'
+          imgSrc='https://picsum.photos/200/300'
+          imgAlt='summer'
+        />
+        <Card
+          title='example1'
+          imgSrc='https://picsum.photos/200/300'
+          imgAlt='summer'
+        />
+      </div>
     </div>
   );
 };
