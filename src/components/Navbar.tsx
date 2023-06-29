3a9726ffbc3e0a25ecacf0cddfb420e051112936
Ian Flynn <ianflynnwork@gmail.com>
2023-06-28 21:46:05 -0400

    add scroll linked background change to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from 'react';
+
 const Navbar: React.FC = () => {
+  //use useState to hold background color for navbar
+  const [navBackgroundColor, setNavBackgroundColor] = useState('transparent');
+  const [navTextColor, setNavTextColor] = useState('--light-font');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 30) {
+        //set nav to white with dark font
+        setNavTextColor('var(--dark-font)');
+        setNavBackgroundColor('var(--nav-background)');
+      } else {
+        //set nav to transparent with light font
+        setNavTextColor('var(--light-font)');
+        setNavBackgroundColor('transparent');
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    //cleanup
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header id='navbar'>
+    <header
+      id='navbar'
+      style={{ backgroundColor: navBackgroundColor, color: navTextColor }}
+    >
       <div id='navbar-logo-container'>
         {/* TO DO: add logo when available */}
         {/* <img src='' id='navbar-logo' /> */}
