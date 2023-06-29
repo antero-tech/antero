b901c99138557d715d7e99edc1c56188d0bb04c4
Ian Flynn <ianflynnwork@gmail.com>
2023-06-29 12:19:14 -0400

    setting up root route for tanstack
    Co-authored-by: Jeb Stone <jeb-stone@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { RootRoute } from '@tanstack/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
