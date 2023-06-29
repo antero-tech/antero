e1a302210a294d8ff651358d965006ff2420461d
Ian Flynn <ianflynnwork@gmail.com>
2023-06-29 16:52:18 -0400

    added TS interface on this comp

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -1,41 +1,30 @@
 import Card from './Card';
 
-const workData = [
-  {
-    title: 'example1',
-    imgSrc: 'https://picsum.photos/200/300',
-    imgAlt: 'summer',
-  },
-  {
-    title: 'example2',
-    imgSrc: 'https://picsum.photos/200/300',
-    imgAlt: 'summer',
-  },
-  {
-    title: 'example3',
-    imgSrc: 'https://picsum.photos/200/300',
-    imgAlt: 'summer',
-  },
-  {
-    title: 'example4',
-    imgSrc: 'https://picsum.photos/200/300',
-    imgAlt: 'summer',
-  },
-];
+interface OurWorkProps {
+  workData: {
+    title: string;
+    imgSrc: string;
+    imgAlt: string;
+  }[];
+}
 
-const OurWork: React.FC = () => {
-  const workCards = workData.map((work) => (
-    <Card
-      key={work.title}
-      title={work.title}
-      imgSrc={work.imgSrc}
-      imgAlt={work.imgAlt}
-    />
-  ));
+const OurWork: React.FC<OurWorkProps> = ({ workData }) => {
+  const workCards = workData
+    .slice(0, 2)
+    .map((work) => (
+      <Card
+        key={work.title}
+        title={work.title}
+        imgSrc={work.imgSrc}
+        imgAlt={work.imgAlt}
+      />
+    ));
   return (
     <div id='our-work'>
-      <h2>our work here</h2>
+      <h2>Sample of our work...</h2>
       <div className='card-container'>{workCards}</div>
+      {/* TO DO: turn this into a link */}
+      <button>See more</button>
     </div>
   );
 };
