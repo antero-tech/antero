c8721373b68131ab6d8781ded02864190f9b77d9
Ian Flynn <ianflynnwork@gmail.com>
2023-06-29 14:42:15 -0400

    setup tanstack router, defined routes, register router and rendered it in app
    Co-authored-by: Jeb Stone <jeb-stone@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,56 @@
 import './App.css';
-import AboutUs from './components/AboutUs';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import OurWorkPage from './pages/OurWorkPage';
+
 import Navbar from './components/Navbar';
-import OurWork from './components/OurWork';
+import { RouterProvider, Router, Route, RootRoute } from '@tanstack/router';
+
+//defines root route and creates outlet for children to be render within
+const rootRoute = new RootRoute({
+  component: Navbar,
+});
+
+//defines child routes
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: HomePage,
+});
+const ourWorkRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: 'our-work',
+  component: OurWorkPage,
+});
+const contactUsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: 'contact-us',
+  component: ContactPage,
+});
+// TO DO: add sub route for individual works within our-work route
+
+//defines route tree assembly
+//TO DO: add children route on our work route that handles individual work
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  ourWorkRoute,
+  contactUsRoute,
+]);
+
+// create router instance
+const router = new Router({ routeTree });
+
+//registering router types
+//explained here https://tanstack.com/router/v1/docs/guide/routes#registering-router-types
+declare module '@tanstack/router' {
+  interface Register {
+    // This infers the type of our router and registers it across the entire project
+    router: typeof router;
+  }
+}
 
 function App() {
-
-  return (
-    <>
-      
-      {/* header */}
-      <Navbar />
-      {/* main */}
-      <main>
-        <Hero />
-        <AboutUs />
-        <OurWork />
-        <Contact />
-      </main>
-      {/* footer */}
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
