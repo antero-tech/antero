fc8c2ba0f237218d8085a8deeda682f03b39939f
Ian Flynn <ianflynnwork@gmail.com>
2023-06-29 14:42:50 -0400

    changed strictmode import to a cleaner syntax
    Co-authored-by: Jeb Stone <jeb-stone@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import { RootRoute } from '@tanstack/router';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
