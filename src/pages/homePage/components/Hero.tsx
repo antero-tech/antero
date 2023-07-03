5c63d625ca234083235d2bb1021442b33fc32e0c
Ian Flynn <ianflynnwork@gmail.com>
2023-07-02 20:18:22 -0400

    cleanup homepage hero

--- src/pages/homePage/components/Hero.tsx
+++ src/pages/homePage/components/Hero.tsx
@@ -1,37 +1,6 @@
-import { useCallback } from 'react';
-// Particles imports
-import Particles from 'react-particles';
-import { loadFull } from 'tsparticles';
-import { Engine } from 'tsparticles-engine';
-import loginParticles from '../../../assets/login-particles.json';
-import { loadFirePreset } from 'tsparticles-preset-fire';
-
 const Hero = () => {
-  // Initialze background particles
-  // const particlesInit = useCallback(async (engine: Engine): Promise<void> => {
-  //   await loadFull(engine);
-  // }, []);
-
-  // const fireInit = useCallback(async (engine: Engine): Promise<void> => {
-  //   await loadFirePreset(engine);
-  // }, []);
-  // const fireOptions: any = {
-  //   preset: 'fire',
-  //   fullScreen: {
-  //     enable: true,
-  //   },
-  //   particles: {
-  //     move: {
-  //       enable: true,
-  //       speed: 0.5,
-  //     },
-  //   },
-  // };
-
-  //const particleOptions: any = loginParticles;
   return (
-    <div id='hero'>
-      {/* <Particles id='particles' options={fireOptions} init={fireInit} /> */}
+    <div id='home-page-hero' className='heros'>
       <div id='hero-text'>
         <h1>Brutally Simple</h1>
         <h1>Dynamic Digital Solutions</h1>
