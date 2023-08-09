1b570f5a24264312b9fd3ff1efcb16d925039849
Jeb Stone <jeb.b.stone@gmail.com>
2023-08-09 14:06:04 -0700

    Updates copy on homepage - subheadings in aboutUsDetails

--- src/pages/homePage/components/AboutUs.tsx
+++ src/pages/homePage/components/AboutUs.tsx
@@ -3,11 +3,10 @@ import AboutDetail from './AboutDetail';
 
 const aboutUsDetails = [
   {
-    heading: 'Scalable Solutions',
-    subheading1: 'Lorem Ipsum',
+    heading: 'Scalable Solutions',    
     copy1:
       'Say goodbye to limitations and hello to limitless possibilities. Our scalable solutions grow and adapt with your business. Whether you need a simple landing page or a complex e-commerce platform',
-    subheading2: 'Lorem Ipsum',
+    subheading2: "Prepare for growth..",
     copy2:
       'Our flexible architecture ensures seamless scalability to handle increasing traffic, expanding content, and evolving user demands. We will future-proof your online presence and empower you to meet your business goals. Experience the freedom to dream big!',
     imgSrc: 'https://picsum.photos/400/400',
@@ -15,10 +14,9 @@ const aboutUsDetails = [
   },
   {
     heading: "It's About the User",
-    subheading1: 'Lorem Ipsum',
     copy1:
       'At Antero, we are passionate about crafting unforgettable user experiences (UX) that leave a lasting impact. Our team of expert designers meticulously analyze user behaviors, needs, and expectations to design an incredible UX that captivates and engages your audience. We prioritize user-centric design principles, ensuring intuitive navigation, seamless interactions, and visually stunning interfaces.',
-    subheading2: 'Lorem Ipsum',
+    subheading2: 'Stand Out in the Crowd..',
     copy2:
       'By incorporating the latest UX trends and best practices, we create a harmonious blend of aesthetics and functionality that keeps users coming back for more. With our meticulous attention to detail and dedication to creating exceptional UX, we empower your brand to stand out in a crowded digital landscape. Trust us to design an unforgettable user experience that leaves a lasting impression on your visitors.',
     imgSrc: 'https://picsum.photos/401/401',
@@ -26,12 +24,11 @@ const aboutUsDetails = [
   },
   {
     heading: 'Service is Our Top Priority',
-    subheading1: 'Lorem Ipsum',
     copy1:
-      'We believe that effective communication and meeting deadlines are crucial elements of successful application development. We prioritize open and transparent communication with our clients throughout the entire process, ensuring that their vision is understood and incorporated into every aspect of the development journey.',
-    subheading2: 'Lorem Ipsum',
+      'We believe that effective communication and meeting deadlines are crucial elements of successful application development. We prioritize open and transparent communication with you throughout the entire process, ensuring that your vision is understood and incorporated into every aspect of the development journey.',
+    subheading2: "Delivered On Time..",
     copy2:
-      'Our dedicated team works closely with clients, providing regular updates, gathering feedback, and addressing any concerns promptly. With our strong commitment to timely delivery, we meticulously plan and manage our development process to meet agreed-upon deadlines. Trust us to not only deliver exceptional applications but also maintain clear communication channels to keep you informed and involved every step of the way.',
+      'Our dedicated team works closely with yours, providing regular updates, gathering feedback, and addressing any concerns promptly. With our strong commitment to timely delivery, we meticulously plan and manage our development process to meet agreed-upon deadlines. Trust us to not only deliver exceptional applications but also maintain clear communication channels to keep you informed and involved every step of the way.',
     imgSrc: 'https://picsum.photos/402/402',
     imgAlt: 'technology',
   },
