d2f3b3ee56caf9f97c0f7551c9b74cff59c8f599
Ian Flynn <ianflynnwork@gmail.com>
2023-08-14 20:52:14 -0400

    changed to typescript file, added interface, and changed export

--- src/assets/work-cards-data.ts
+++ src/assets/work-cards-data.ts
@@ -1,11 +1,25 @@
-import junoSurfMock from './our-work/juno-phone-mockup.png';
+import junoSurf from './our-work/juno-phone-mockup.png';
 
-const workData = [
+interface WorkData {
+  title: string;
+  description: string;
+  imgSrc: string;
+  imgAlt: string;
+}
+
+const workData: WorkData[] = [
   {
     title: 'Friendify',
     description:
       'Discover new music and find new friends based on what you are currenly listening to. Friendify connects audiophiles from all over the world!',
     imgSrc: 'https://picsum.photos/500/500',
+    imgAlt: 'summer',
+  },
+  {
+    title: 'kafkAlerts',
+    description:
+      'Connect to your Kafka clusters in seconds. Monitor broker metrics and receive alerts based on service level objectives.',
+    imgSrc: 'https://picsum.photos/500/604',
     imgAlt: 'summer',
   },
   {
@@ -19,15 +33,9 @@ const workData = [
     title: 'Juno Surf',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec al',
-    imgSrc: junoSurfMock,
-    imgAlt: 'summer',
-  },
-  {
-    title: 'kafkAlerts',
-    description:
-      'Connect to your Kafka clusters in seconds. Monitor broker metrics and receive alerts based on service level objectives.',
-    imgSrc: 'https://picsum.photos/500/604',
-    imgAlt: 'summer',
+    imgSrc: junoSurf,
+    imgAlt: 'Juno Surf',
   },
 ];
+
 export default workData;
